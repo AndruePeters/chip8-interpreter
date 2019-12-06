b35bf39b09dd7411890ff039849d81a4b19b74f3
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-12-05 19:25:12 -0500

    Adding basic clases and opcodes. Will refactor

--- include/chip8cpu.h
+++ include/chip8cpu.h
@@ -6,9 +6,46 @@
 #include <cstdint>
 
 
-namespace CHiP8 {
+namespace chip8 {
 
+template <RegisterWidth W, RegisterSize S>
+class Register {
 
+};
+
+template <MemoryWidth W, MemorySize S>
+class Memory {
+
+};
+
+template <StackWidth W, StackSize S>
+class Stack {
+
+};
+
+enum class OPCodeMask: uint16_t {
+    CLS = 0x00E0,
+    RET = 00EE,
+    GOTO = 0x1000,
+    JMP_I = 0x2000,
+    EQ_C = 0x3000,
+    NEQ_C = 0x4000,
+    EQ = 0x5000,
+    SET_I = 0x6000,
+    ADD_I = 0x7000,
+    SET = 0x8000,
+    OR = 0x8001,
+    AND = 0x8002,
+    XOR = 0x8003,
+    ADD = 0x8004,
+    SUB = 0x8005,
+    SRL = 0x8006,
+    SUB_R = 0x8007,
+    SLL = 0x800E,
+
+};
+
+template <Reg R, Mem M, Stack S>
 class cpu {
 
     std::array<uint8_t, 4096> ram_;
@@ -24,5 +61,5 @@ class cpu {
 
 
 
-}
+} // end namespace chip8
 #endif
