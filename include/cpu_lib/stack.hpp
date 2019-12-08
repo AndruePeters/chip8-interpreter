8c2fdcb7a3b76503155f508c884564735e9f826c
root <5511409+AndruePeters@users.noreply.github.com>
2019-12-08 00:16:02 -0500

    Basic stack wrapper to emulate a hardware stack

--- include/cpu_lib/stack.hpp
+++ include/cpu_lib/stack.hpp
@@ -3,19 +3,44 @@
 
 #include <algorithm>
 #include <array>
+#include <stack>
 
 namespace cpu_lib {
 
-template <typename StackType, class StackCont = std::array<W>>
+template <class StackType, std::size_t s>
 class Stack {
 public:
-    auto pop() const;
-    auto peek() const;
-    void push(auto const val);
-    void dump() const;
+    [[nodiscard]] constexpr auto pop();
+    [[nodiscard]] constexpr auto peek() const;
+    void constexpr push(const StackType val);
+    [[nodiscard]] constexpr auto size() const {return s;}
 protected:
-    StackCont<StackType> stack;
+    std::stack<StackType> stack;
 }; // end class Stack
+
+template <class StackType, std::size_t s>
+constexpr auto Stack<StackType, s>::pop()
+{
+    return stack.front();
+}
+
+template <class StackType, std::size_t s>
+constexpr auto Stack<StackType, s>::peek() const
+{
+    auto x = stack.front();
+    stack.pop_front();
+    return x;
+}
+
+template <class StackType, std::size_t s>
+constexpr void Stack<StackType, s>::push(const StackType val)
+{
+    stack.push_front(val);
+}
+
+
+
+
 
 } // end namespace cpu_lib
 #endif
