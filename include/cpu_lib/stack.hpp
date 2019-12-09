a8dfb079a16bb562421fb619cb4d484303b43d02
root <5511409+AndruePeters@users.noreply.github.com>
2019-12-08 20:11:59 -0500

    Doxygen

--- include/cpu_lib/stack.hpp
+++ include/cpu_lib/stack.hpp
@@ -7,29 +7,61 @@
 
 namespace cpu_lib {
 
+
+/**
+ * @class Stack
+ * @brief Represents internal CPU stack. 
+ * 
+ * @tparam StackType    Should be unsigned integral type representing
+ *                      the stack bitwidth. For example, if the stack consists
+ *                      of slots 8-bits wide, then use uint8_t.
+ *                      Will convert to a static size in the future.
+ * 
+ * @tparam s            Depth of stack. If it's 16 bytes, then you'd use 16 for s and uint8_t for StackType.
+ */
 template <class StackType, std::size_t s>
 class Stack {
 public:
+
+    /**
+     * @return Returns and removes top element of stack.
+     */
     [[nodiscard]] constexpr auto pop();
+
+    /**
+     * @return Top element of stack without removing it.
+     */
     [[nodiscard]] constexpr auto peek() const;
+
+    /**
+     * @brief Pushes \p val to stack.
+     */
     void constexpr push(const StackType val);
+
+    /**
+     * @return Size of stack, template parameter s
+     */
     [[nodiscard]] constexpr auto size() const {return s;}
 protected:
+
+    /**
+     * Stack
+     */
     std::stack<StackType> stack;
 }; // end class Stack
 
 template <class StackType, std::size_t s>
 constexpr auto Stack<StackType, s>::pop()
 {
-    return stack.front();
+    auto front = stack.front();
+    stack.pop_front();
+    return front;
 }
 
 template <class StackType, std::size_t s>
 constexpr auto Stack<StackType, s>::peek() const
 {
-    auto x = stack.front();
-    stack.pop_front();
-    return x;
+    return stack.front();
 }
 
 template <class StackType, std::size_t s>
