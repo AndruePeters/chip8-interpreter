249baa83d2d91927102201f059b595f031ed0788
root <5511409+AndruePeters@users.noreply.github.com>
2019-12-10 17:40:30 -0500

    Added invalid_set and invalid_get

--- test/memory_unit_test.cpp
+++ test/memory_unit_test.cpp
@@ -7,9 +7,41 @@
 
 using namespace cpu_lib;
 
-TEST(MemoryUnitTest, valid_set_get)
+TEST(MemoryUnitTest, valid_get_and_set)
 {
     Memory<uint32_t, 256> memory;
     memory.set(0x00, 0x12);
     EXPECT_EQ(static_cast<uint32_t>(0x12), memory.get(0x00));
 }
+
+TEST(MemoryUnitTest, invalid_set)
+{
+    Memory<uint32_t, 256> memory;
+    try {
+        memory.set(256)
+        FAIL() << "Expected std::out_of_range.";
+    }
+    catch(std::out_of_range const & err) {
+        EXPECT_EQ(err.what(),std::string("Memory::set() : address is out of range."));
+    }
+    catch(...) {
+        FAIL() << "Expected std::out_of_range.";
+    }
+}
+
+TEST(MemoryUnitTest, invalid_get)
+{
+    Memory<uint32_t, 256> memory;
+    try {
+        memory.get(256)
+        FAIL() << "Expected std::out_of_range.";
+    }
+    catch(std::out_of_range const & err) {
+        EXPECT_EQ(err.what(),std::string("Memory::get() : address is out of range."));
+    }
+    catch(...) {
+        FAIL() << "Expected std::out_of_range.";
+    }
+}
+
+
