4e45110d772f3d8cf54a476c0c7cfc3211cc726a
root <5511409+AndruePeters@users.noreply.github.com>
2019-12-09 19:03:26 -0500

    Small example with fmt to verify and test cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+
+
+#include <fmt/printf.h>
+
+
+#include <cpu_lib/memory.hpp>
+#include <cpu_lib/register.hpp>
+#include <cpu_lib/stack.hpp>
+
 int main()
 {
-    std::cout << "Hello world" << std::endl;
+    fmt::printf("Hello, %s!\n", "world");
     return 0;
 }
