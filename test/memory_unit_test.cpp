ff863e99f132f66e611c63457cb1e038c0ad981b
root <5511409+AndruePeters@users.noreply.github.com>
2019-12-10 22:15:47 -0500

    Line to test static_assert when Size is = 0

--- test/memory_unit_test.cpp
+++ test/memory_unit_test.cpp
@@ -7,13 +7,19 @@
 
 using namespace cpu_lib;
 
+/**
+ * @brief Simple test for verifying get and set work properly.
+ */
 TEST(MemoryUnitTest, valid_get_and_set)
 {
     Memory<uint32_t, 256> memory;
     memory.set(0x00, 0x12);
-    EXPECT_EQ(static_cast<uint32_t>(0x12), memory.get(0x00));
+    ASSERT_EQ(static_cast<uint32_t>(0x12), memory.get(0x00));
 }
 
+/**
+ * @brief Test designed to throw std::out_of_range("Memory::set() : address is out of range.") in order to pass.
+ */
 TEST(MemoryUnitTest, invalid_set)
 {
     Memory<uint32_t, 256> memory;
@@ -22,13 +28,16 @@ TEST(MemoryUnitTest, invalid_set)
         FAIL() << "Expected std::out_of_range.";
     }
     catch(std::out_of_range const & err) {
-        EXPECT_EQ(err.what(),std::string("Memory::set() : address is out of range."));
+        ASSERT_EQ(err.what(),std::string("Memory::set() : address is out of range."));
     }
     catch(...) {
         FAIL() << "Expected std::out_of_range.";
     }
 }
 
+/**
+ * @brief Test designed to throw std::out_of_range("Memory::get() : address it out of range.") in order to pass.
+ */
 TEST(MemoryUnitTest, invalid_get)
 {
     Memory<uint32_t, 256> memory;
@@ -37,11 +46,24 @@ TEST(MemoryUnitTest, invalid_get)
         FAIL() << "Expected std::out_of_range.";
     }
     catch(std::out_of_range const & err) {
-        EXPECT_EQ(err.what(),std::string("Memory::get() : address it out of range."));
+        ASSERT_EQ(err.what(),std::string("Memory::get() : address it out of range."));
     }
     catch(...) {
         FAIL() << "Expected std::out_of_range.";
     }
+}
+
+TEST(MemoryUnitTest, size)
+{
+    Memory<uint32_t, 256> mem1;
+    Memory<uint8_t, 128> mem2;
+    Memory<unsigned , 1000> mem3;
+    ASSERT_EQ(256, mem1.size())     << "Expected mem1.size() to be 256.";
+    ASSERT_EQ(128, mem2.size())     << "Expected mem2.size() to be 128.";
+    ASSERT_EQ(1000, mem3.size())    << "Expected mem3.size() to be 1000.";
+
+    // uncomment this line to test static_assert. Program should fail to compile. 
+    //Memory<unsigned, 0> mem4;
 }
 
 
