9ebb31209a784833870a92a629bb25e75577e1c5
root <5511409+AndruePeters@users.noreply.github.com>
2019-12-11 21:17:20 -0500

    Changed API and fixed issues

--- include/cpu_lib/stack.hpp
+++ include/cpu_lib/stack.hpp
@@ -17,31 +17,41 @@ namespace cpu_lib {
  *                      of slots 8-bits wide, then use uint8_t.
  *                      Will convert to a static size in the future.
  * 
- * @tparam s            Depth of stack. If it's 16 bytes, then you'd use 16 for s and uint8_t for StackType.
+ * @tparam Size            Depth of stack. If it's 16 bytes, then you'd use 16 for s and uint8_t for StackType.
  */
-template <class StackType, std::size_t s>
+template <class StackType, std::size_t SizeLimit>
 class Stack {
 public:
     /**
-     * @return Returns and removes top element of stack.
+     * SizeLimit can neber be 0 or less.
      */
-    [[nodiscard]] constexpr auto pop();
+    static_assert(SizeLimit > 0, "In class Stack, template-parameter SizeLimit must be greater than 0.");
+    
+    /**
+     * @brief Removes the top element of the stack.
+     */
+    void pop();
 
     /**
      * @return Top element of stack without removing it.
      */
-    [[nodiscard]] constexpr auto peek() const;
+    [[nodiscard]] auto top() const;
 
     /**
      * @brief Pushes \p val to stack.
      */
-    void constexpr push(const StackType val);
+    void push(const StackType val);
 
     /**
-     * @return Size of stack, template parameter s
+     * @return Size of stack, template parameter SizeLimit
      */
-    [[nodiscard]] constexpr auto size() const {return s;}
-    
+    [[nodiscard]] auto sizeLimit() const { return SizeLimit; }
+
+    /**
+     * @return Size of the underlying stack container.
+     */ 
+    [[nodiscard]] auto sizeUsed() const { return stack.size(); }
+
 protected:
     /**
      * Stack
@@ -49,24 +59,31 @@ protected:
     std::stack<StackType> stack;
 }; // end class Stack
 
-template <class StackType, std::size_t s>
-constexpr auto Stack<StackType, s>::pop()
+template <class StackType, std::size_t SizeLimit>
+void Stack<StackType, SizeLimit>::pop()
 {
-    auto front = stack.front();
-    stack.pop_front();
-    return front;
+    if ( this->sizeUsed() == 0) {
+        throw std::out_of_range("Stack::pop() : stack is empty. Cannot pop stack.");
+    }
+    stack.pop();
 }
 
-template <class StackType, std::size_t s>
-constexpr auto Stack<StackType, s>::peek() const
+template <class StackType, std::size_t SizeLimit>
+auto Stack<StackType, SizeLimit>::top() const
 {
-    return stack.front();
+    if (this->sizeUsed() == 0) {
+        throw std::out_of_range("Stack::top() : stack is empty. Cannot get top.");
+    }
+    return stack.top();
 }
 
-template <class StackType, std::size_t s>
-constexpr void Stack<StackType, s>::push(const StackType val)
+template <class StackType, std::size_t SizeLimit>
+void Stack<StackType, SizeLimit>::push(const StackType val)
 {
-    stack.push_front(val);
+    if (( this->sizeUsed() + 1) > SizeLimit) {
+        throw std::out_of_range("Stack::push() : pushed more than SizeLimit.");
+    }
+    stack.push(val);
 }
 
 
