3539d9943ac4a21f37a35f9d90f9b65bfa731d46
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2020-09-08 13:28:34 -0400

    add imgui basics

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,55 @@
 #include <cpu_lib/register.hpp>
 #include <cpu_lib/stack.hpp>
 
+#include <imgui.h>
+#include <imgui-SFML.h>
+
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+
 int main()
 {
+
     fmt::printf("Hello, %s!\n", "world");
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 4;
+    sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3",  sf::Style::Default, settings);
+    window.setFramerateLimit(60);
+    ImGui::SFML::Init(window);
+
+    sf::CircleShape shape(100.f);
+    shape.setPointCount(999999);
+    shape.setFillColor(sf::Color::Green);
+    shape.setOutlineColor(sf::Color::Cyan);
+
+    sf::Clock deltaClock;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(event);
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+//    ImGui::ShowDemoWindow();
+
+        ImGui::Begin("Hello, world!");
+        ImGui::Button("Look at this pretty button");
+        ImGui::End();
+
+        window.clear();
+        window.draw(shape);
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
+
     return 0;
 }
