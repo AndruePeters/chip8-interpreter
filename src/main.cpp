2c3d31e31fd245aef16b1fe35b22e67831a62e26
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2020-09-12 17:16:24 -0400

    code to dump ram/stack

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
-#include <iostream>
-
-
 #include <fmt/printf.h>
 
 
 #include <cpu_lib/memory.hpp>
 #include <cpu_lib/register.hpp>
 #include <cpu_lib/stack.hpp>
+#include <chip8cpu.h>
 
 #include <imgui.h>
 #include <imgui-SFML.h>
@@ -16,47 +14,59 @@
 #include <SFML/Window/Event.hpp>
 #include <SFML/Graphics/CircleShape.hpp>
 
+
 int main()
 {
+  chip8::CPU cpu;
+  sf::ContextSettings settings;
+  settings.antialiasingLevel = 1;
+  sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3", sf::Style::Default, settings);
+  window.setFramerateLimit(60);
+  ImGui::SFML::Init(window);
 
-    fmt::printf("Hello, %s!\n", "world");
-    sf::ContextSettings settings;
-    settings.antialiasingLevel = 4;
-    sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3",  sf::Style::Default, settings);
-    window.setFramerateLimit(60);
-    ImGui::SFML::Init(window);
+  sf::CircleShape shape(100.f);
+  shape.setPointCount(1000);
+  shape.setFillColor(sf::Color::Green);
+  shape.setOutlineColor(sf::Color::Cyan);
+  sf::Clock deltaClock;
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      ImGui::SFML::ProcessEvent(event);
 
-    sf::CircleShape shape(100.f);
-    shape.setPointCount(999999);
-    shape.setFillColor(sf::Color::Green);
-    shape.setOutlineColor(sf::Color::Cyan);
-
-    sf::Clock deltaClock;
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            ImGui::SFML::ProcessEvent(event);
-
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
-
-        ImGui::SFML::Update(window, deltaClock.restart());
-
-//    ImGui::ShowDemoWindow();
-
-        ImGui::Begin("Hello, world!");
-        ImGui::Button("Look at this pretty button");
-        ImGui::End();
-
-        window.clear();
-        window.draw(shape);
-        ImGui::SFML::Render(window);
-        window.display();
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
     }
 
-    ImGui::SFML::Shutdown();
+    ImGui::SFML::Update(window, deltaClock.restart());
 
-    return 0;
+
+    ImGui::Begin("Hello, world!");
+    ImGui::Button("Look at this pretty button");
+    ImGui::End();
+
+    ImGui::Begin("ram");
+      ImGui::Columns(8);
+        for (const auto ramVal : cpu.ram_) {
+          ImGui::Text("0x%.2X", ramVal);
+          ImGui::NextColumn();
+        }
+    ImGui::End();
+
+    ImGui::Begin("call stack");
+      for (const auto stackVal : cpu.call_stack_) {
+        ImGui::Text("0x%.4X", stackVal);
+      }
+    ImGui::End();
+
+    window.clear();
+    //window.draw(shape);
+    ImGui::SFML::Render(window);
+    window.display();
+  }
+
+  ImGui::SFML::Shutdown();
+
+  return 0;
 }
